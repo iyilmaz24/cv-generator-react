adf710bab46266f891a134981ca1191f110d0e0c
Irfan Yilmaz <iy.usa@outlook.com>
2023-12-25 22:54:03 -0500

    simplify solution, implement across all text inputs in file

--- src/components/personalDetails.jsx
+++ src/components/personalDetails.jsx
@@ -7,18 +7,12 @@ export default function PersonalDetails({  styleClasses, children, shown, functi
             <>
                 <div id="personal-details-form" className={styleClasses + " component-form-shown"}>
                     <form>
-                        <input placeholder="First Name" onFocus={   (event.target.value == "") ? 
-                        // right now if the user focuses on the default input, it will disappear which is good
-                            // but on the false for the ternary operator (?) we should return false, null or 
-                            // something similar rather than invalidly calling the useState setter, it makes the app crash
-                            functions.setFirstName("") : functions.setFirstName
-                        } 
-                        onChange={(event) => functions.setFirstName(event.target.value)}></input>
-                        <input placeholder="Last Name" onChange={(event) => functions.setLastName(event.target.value)}></input>
-                        <input placeholder="Phone #" onChange={(event) => functions.setPhoneNum(event.target.value)}></input>
-                        <input placeholder ="Email" onChange={(event) => functions.setEmail(event.target.value)}></input>
-                        <input placeholder="City, State" onChange={(event) => functions.setLocation(event.target.value)}></input>
-                        <input placeholder="Current Title" onChange={(event) => functions.setCurrTitle(event.target.value)}></input>
+                        <input placeholder="First Name" onFocus={(event) => functions.setFirstName(event.target.value)} onChange={(event) => functions.setFirstName(event.target.value)}></input>
+                        <input placeholder="Last Name" onFocus={(event) => functions.setLastName(event.target.value)} onChange={(event) => functions.setLastName(event.target.value)}></input>
+                        <input placeholder="Phone #"  onFocus={(event) => functions.setPhoneNum(event.target.value)} onChange={(event) => functions.setPhoneNum(event.target.value)}></input>
+                        <input placeholder ="Email" onFocus={(event) => functions.setEmail(event.target.value)} onChange={(event) => functions.setEmail(event.target.value)}></input>
+                        <input placeholder="City, State" onFocus={(event) => functions.setLocation(event.target.value)} onChange={(event) => functions.setLocation(event.target.value)}></input>
+                        <input placeholder="Current Title" onFocus={(event) => functions.setCurrTitle(event.target.value)} onChange={(event) => functions.setCurrTitle(event.target.value)}></input>
                     </form>
                     <div className="form-title">
                         { children }
@@ -42,3 +36,9 @@ export default function PersonalDetails({  styleClasses, children, shown, functi
         )
     }
 }
+
+// (event.target.value == "") ? 
+// // right now if the user focuses on the default input, it will disappear which is good
+//     // but on the false for the ternary operator (?) we should return false, null or 
+//     // something similar rather than invalidly calling the useState setter, it makes the app crash
+//     functions.setFirstName("") : console.log(event.target.value)
