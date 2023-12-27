909e60d8d155f77f1517940575b46eb5189fc66d
Irfan Yilmaz <iy.usa@outlook.com>
2023-12-26 19:40:18 -0500

    implement deletion of previously inputted links

--- src/components/additionalDetails.jsx
+++ src/components/additionalDetails.jsx
@@ -13,11 +13,22 @@ export default function AdditionalDetails({ styleClasses, children, shown, funct
                 <div className={styleClasses + " component-form-shown"}>
                     <form>
                         <input placeholder="LinkedIn" onChange={(e) => {
-                            setLinkedIn(e.target.value); linkedInNode.style.display = "flex"}}></input>
+                            setLinkedIn(e.target.value); if(e.target.value != ""){linkedInNode.style.display = "flex"}
+                            else{linkedInNode.style.display = "none"}}} onFocus={(e) => {
+                                setLinkedIn(e.target.value); if(e.target.value != ""){linkedInNode.style.display = "flex"}
+                                else{linkedInNode.style.display = "none"}}}></input>
+
                         <input placeholder="GitHub" onChange={(e) => {
-                            setGitHub(e.target.value); gitHubNode.style.display = "flex"}}></input>
+                            setGitHub(e.target.value); if(e.target.value != ""){gitHubNode.style.display = "flex"}
+                            else{gitHubNode.style.display = "none"}}} onFocus={(e) => {
+                                setGitHub(e.target.value); if(e.target.value != ""){gitHubNode.style.display = "flex"}
+                                else{gitHubNode.style.display = "none"}}}></input>
+
                         <input placeholder="Website/Other" onChange={(e) => {
-                            setOtherSite(e.target.value); otherSiteNode.style.display = "flex"}}></input>
+                            setOtherSite(e.target.value); if(e.target.value != ""){otherSiteNode.style.display = "flex"}
+                            else{otherSiteNode.style.display = "none"}}} onFocus={(e) => {
+                                setOtherSite(e.target.value); if(e.target.value != ""){otherSiteNode.style.display = "flex"}
+                                else{otherSiteNode.style.display = "none"}}}></input>
                     </form>
                     <div className="form-title">
                         { children }
